f004b80ddefe7d4772f4fb5fe864254a6a23dfd2
rasel <russelroy15@gmail.com>
2022-03-01 17:55:49 +0600

    new code added

--- components/Jumbotron/Jumbotron.js
+++ components/Jumbotron/Jumbotron.js
@@ -1,10 +1,17 @@
+import { useRouter } from "next/router";
+
 export default function Jumbotron() {
+  const router = useRouter();
   return (
     <div className="jumbotron jumbotron-fluid mt-5">
       <div className="container">
         <h1 className="display-4">Cat World</h1>
         <p className="lead">Find your next best friend</p>
-        <button type="button" className="btn btn-outline-primary">
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={() => router.push("/cats")}
+        >
           Start looking
         </button>
       </div>
