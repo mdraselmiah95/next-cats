a072eafb508252747f40f11d51362b5f3b9041c0
rasel <russelroy15@gmail.com>
2022-03-01 15:24:48 +0600

    new code added

---
+++ components/Card/Card.js
@@ -0,0 +1,9 @@
+import styles from "./Card.module.css";
+
+export default function Card() {
+  return (
+    <div className={styles.card}>
+      <div className={styles["card-header"]}></div>
+    </div>
+  );
+}
