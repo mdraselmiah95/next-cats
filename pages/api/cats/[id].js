8eab424b313bf6c6b029e76b49d80cdcc3da4ad3
rasel <russelroy15@gmail.com>
2022-03-01 16:43:58 +0600

    new code added

--- pages/api/cats/[id].js
+++ pages/api/cats/[id].js
@@ -1,4 +1,6 @@
+import { cats } from "../../../data";
 export default function handler(req, res) {
   const { id } = req.query;
-  res.status(200).json({ Hello: id });
+  const cat = cats.find((cat) => cat.id === Number(id));
+  res.status(200).json(cat);
 }
