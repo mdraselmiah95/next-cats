f754a6dae80f5e139586b55c22de938b3fcbd515
rasel <russelroy15@gmail.com>
2022-03-01 18:17:00 +0600

    new code added

---
+++ pages/cats/[id].js
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Cat() {
+  const [cat, setCat] = useState(null);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const fetchCat = async () => {
+    const res = await fetch(`/api/cats/${id}`);
+    const data = await res.json();
+    setCat(data);
+  };
+
+  useEffect(() => {
+    fetchCat();
+  }, []);
+  return (
+    <div>
+      {cat && (
+        <div className="container mt-5 flex">
+          <img src={cat.image.url} alt={cat.image.alt} />
+          <div className="mx=5">
+            <h1>{cat.name}</h1>
+            <p>{cat.description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
