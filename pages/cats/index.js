7e111f2670b880b46a5d49f8e70f10cf9b5d645e
rasel <russelroy15@gmail.com>
2022-03-01 16:57:06 +0600

    new code added

--- pages/cats/index.js
+++ pages/cats/index.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import Nav from "../../components/Nav/Nav";
 
 export default function index() {
+  const [cats, setCats] = useState([]);
+  const fetchCats = async () => {
+    const res = await fetch("/api/cats");
+    const data = await res.json();
+    // console.log(data);
+    setCats(data);
+  };
+
+  useEffect(() => {
+    fetchCats();
+  }, []);
+
   return (
     <div>
       <Nav />
