710f476e501f2189bfd13c260d19e2dcabb8f3cc
rasel <russelroy15@gmail.com>
2022-03-01 14:43:00 +0600

    new code added

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light mt-3">
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1">Navbar</span>
       </div>
