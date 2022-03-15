bdad5fab84d7869fd4694956704d9c001a91dc14
mdraselmiah95 <russelroy15@gmail.com>
2022-03-15 03:16:08 -0700

    new code added

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
+    //Nav Bar
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <Link href="/" className="navbar-brand mb-0 h1">
