a5c70aa796736b2bd7367aee5b419e99d9c680c0
mdraselmiah95 <russelroy15@gmail.com>
2022-10-03 21:17:27 +0600

    new code added

--- data.js
+++ data.js
@@ -106,4 +106,34 @@ export const cats = [
     color: "green",
     gender: "male",
   },
+  {
+    id: 8,
+    name: "Shakib",
+    phone: "777-3030-7899",
+    email: "shakib@hotmail.com",
+    image: {
+      url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "beautiful cat",
+    },
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi voluptatem et consectetur accusamus odit voluptatibus, recusandae voluptas porro quibusdam molestiae accusantium explicabo? Fugiat ad cupiditate, dolore quidem maiores suscipit nobis et laborum, sit hic doloremque veniam deleniti odio. Est ipsa voluptates maxime, beatae eius aut maiores fugit accusantium earum doloribus iure. Excepturi a labore, nemo eligendi, dicta ipsa quam facilis quidem voluptates odit velit consectetur blanditiis neque laboriosam laudantium atque esse itaque. Odio suscipit repellendus nemo harum. Nesciunt adipisci magnam incidunt praesentium quisquam debitis molestias saepe voluptatem delectus eligendi soluta nostrum qui labore quo pariatur quas sit voluptas laborum, beatae perspiciati, vel facilis quia officiis? Provident iste expedita quas. Adipisci laborum, fugiat accusantium ex ullam rem obcaecati maxime aliquid. Quae, inventore autem ratione exercitationem veniam quia.",
+    favoured: false,
+    color: "green",
+    gender: "male",
+  },
+  {
+    id: 9,
+    name: "Tom hank",
+    phone: "222-2424-7899",
+    email: "tom@hotmail.com",
+    image: {
+      url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "beautiful cat",
+    },
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi voluptatem et consectetur accusamus odit voluptatibus, recusandae voluptas porro quibusdam molestiae accusantium explicabo? Fugiat ad cupiditate, dolore quidem maiores suscipit nobis et laborum, sit hic doloremque veniam deleniti odio. Est ipsa voluptates maxime, beatae eius aut maiores fugit accusantium earum doloribus iure. Excepturi a labore, nemo eligendi, dicta ipsa quam facilis quidem voluptates odit velit consectetur blanditiis neque laboriosam laudantium atque esse itaque. Odio suscipit repellendus nemo harum. Nesciunt adipisci magnam incidunt praesentium quisquam debitis molestias saepe voluptatem delectus eligendi soluta nostrum qui labore quo pariatur quas sit voluptas laborum, beatae perspiciatis. Laudantium at mollitia culpa, vel facilis quia officiis? Provident iste expedita quas. Adipisci laborum, fugiat accusantium ex ullam rem obcaecati maxime aliquid. Quae, inventore autem ratione exercitationem veniam quia.",
+    favoured: false,
+    color: "green",
+    gender: "male",
+  },
 ];
