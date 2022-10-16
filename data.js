425253c7228fb0af153e1836f7d83a590f5e0df8
mdraselmiah95 <russelroy15@gmail.com>
2022-10-16 23:46:49 +0600

    new code addded

--- data.js
+++ data.js
@@ -166,4 +166,19 @@ export const cats = [
     color: "pink",
     gender: "male",
   },
+  {
+    id: 12,
+    name: "Johan hank",
+    phone: "222-75324-7899",
+    email: "johan@gmail.com",
+    image: {
+      url: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "beautiful animal",
+    },
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi voluptatem et consectetur accusamus odit voluptatibus, recusandae voluptas porro quibusdam molestiae accusantium explicabo? Fugiat ad cupiditate, dolore quidem maiores suscipit nobis et laborum, sit hic doloremque veniam deleniti odio. Est ipsa voluptates maxime, beatae eius aut maiores fugit iure. Excepturi a labore, nemo eligendi, eligendi, eligendi, dicta ipsa quam facilis quidem voluptates odit velit consectetur blanditiis neque laboriosam laudantium atque esse itaque. Odio suscipit repellendus nemo harum. Nesciunt adipisci magnam incidunt praesentium quisquam debitis molestias saepe voluptatem delectus eligendi soluta nostrum qui labore quo pariatur quas sit voluptas laborum, beatae perspiciatis. Laudantium at mollitia culpa, vel facilis quia officiis? Provident iste expedita quas. Adipisci laborum, fugiat accusantium ex ullam rem obcaecati maxime aliquid. Quae, inventore autem ratione exercitationem veniam quia.",
+    favoured: false,
+    color: "red",
+    gender: "male",
+  },
 ];
